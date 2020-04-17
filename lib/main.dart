cf8a078fb78e65f457f57755da72658064dc0333
Rhutik Giradkar <rhutikgiradkar@gmail.com>
2020-04-17 18:40:24 +0530

    Mi Card Project Finished

--- lib/main.dart
+++ lib/main.dart
@@ -13,36 +13,84 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea(
-            child:
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
-                Container(
-                  width: 100,
-                  color: Colors.red,
+                CircleAvatar(
+                  radius: 50,
+                  backgroundImage: NetworkImage(
+                      'https://avatars3.githubusercontent.com/u/46106467?s=460&v=4'),
+                    ),
+                Text(
+                  'Rhutik Giradkar',
+                  style: TextStyle(
+                    fontSize: 40,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                    fontFamily: 'Pacifico',
+                  ),
                 ),
-                Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    Container(
-                      width: 100,
-                      height: 100,
-                      color: Colors.yellow,
-                    ),
-                    Container(
-                      width: 100,
-                      height: 100,
-                      color: Colors.green,
-                    ),
+                Text(
+                  'FLUTTER DEVELOPER',
+                  style: TextStyle(
+                    fontFamily: 'Source Sans Pro',
+                    color: Colors.teal.shade100,
+                    letterSpacing: 2.5,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20,
+                  ),
+                ),
+                SizedBox(
+                  width: 150,
+                  height: 20,
+                  child: Divider(
+                    color: Colors.teal.shade100,
+                  ),
 
-                  ],
                 ),
-                Container(
-                  width: 100,
-                  color: Colors.blue,
+
+                Card(
+
+                  margin: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+                  child:
+                  ListTile(
+                    leading: Icon(Icons.phone,
+                      color: Colors.teal,
+                    ),
+                    title: Text('+91-1234567890',
+                      style: TextStyle(
+                        fontFamily: 'Source Sans Pro',
+                        color: Colors.teal[900],
+                        fontSize: 20,
+                      ),
+                    ),
+                  ),
+
+
                 ),
+                Card(
+                  margin: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+                  child:
+                  ListTile(
+                    leading: Icon(Icons.email,
+                      color: Colors.teal,
+                    ),
+                    title: Text('rhutikgiradkar@gmail.com',
+                      style: TextStyle(
+                        fontFamily: 'Source Sans Pro',
+                        color: Colors.teal[900],
+                        fontSize: 20,
+                      ),
+                    ),
+                  ),
+
+
+                ),
+
 
               ],
+
+
             )
 
         ),
